3f116899dfbdd613a07b9f2cc23dc53ef0a3ef07
matheusmapa <86133396+matheusmapa@users.noreply.github.com>
2026-02-03 18:30:01 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Garante que o build para produção seja otimizado
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
